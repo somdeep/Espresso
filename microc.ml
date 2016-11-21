46d9e5b9c4732e8f5dfa859de9e24cb3874231b1
Somdeep Dey <sd2988@columbia.edu>
2016-11-20 19:49:25 -0500

    updated todo list and got micro.ml updated for compile flag

--- microc.ml
+++ microc.ml
@@ -15,7 +15,7 @@ let _ =
   match action with
     Ast -> print_string (Ast.string_of_program ast) 
   | LLVM_IR -> print_string (Llvm.string_of_llmodule (Codegen.translate ast))
-(*  | Compile -> let m = Codegen.translate ast in
-    Llvm_analysis.assert_valid_module m;
+  | Compile -> let m = Codegen.translate ast in
+    (*Llvm_analysis.assert_valid_module m;*)
     print_string (Llvm.string_of_llmodule m)
-*)
+
