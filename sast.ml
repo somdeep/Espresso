327d86b5ee36543052466ba60c11e511f8521f0d
Rohit Gurunath <rohitg5249@gmail.com>
2016-11-23 21:43:23 -0500

    added checks to find out duplicate classes, duplicate methods and duplicate fields. added sast. getting syntax error in sast, need to figure it out

--- sast.ml
+++ sast.ml
@@ -14,7 +14,7 @@ type sexpr =
   | SArrayAccess of string * sexpr
   | SNoexpr
 
-type var_decl = Vdecl of typ * string
+(*type var_decl = Vdecl of typ * string*)
 
 type sstmt =
     SBlock of sstmt list
@@ -32,24 +32,24 @@ type sfunc_decl = {
     typ : typ;
     fname : string;
     formals : formal list;
-    body : stmt list;
+    body : sstmt list;
     ftype : ftype;
   }
 
 
 type scbody = {
   fields : var_decl list;
-  methods : func_decl list;
+  methods : sfunc_decl list;
 
 }
 
 type scdecl = {
   cname : string;
-  cbody : cbody;
+  cbody : scbody;
 }
 
 type sprogram =  {
-	class : scdecl ;
+	class : scdecl list;
 	functions : sfunc_decl list;
 	main : sfunc_decl;
 	reserved : sfunc_decl list;
--- semant.ml
+++ semant.ml
@@ -1,8 +1,21 @@
 open Ast
-(* open Sast *)
+(*open Sast *)
 (*open Helper*)
 
 module StringMap = Map.Make (String)
+
+type classMap = {
+	field_map               : Ast.var_decl StringMap.t;
+	func_map               : Ast.func_decl StringMap.t;
+	(*builtFuncMap 	: sfunc_decl StringMap.t;*)
+	cdecl 			        : Ast.cdecl;
+}
+
+let get_fully_qualified_name class_name fdecl = 
+    let func_name = fdecl.fname in
+        match func_name with
+            "main" -> "main"
+            |   _ -> class_name ^ "." ^ func_name
 
 (* define all built-in functions supported by espresso *)
 let get_reserved_funcs = 
@@ -12,14 +25,49 @@ let get_reserved_funcs =
 			typ 	        = return_type;
 			formals 		= formals;
 			body 			= [];
-		(*	func_type		= Sast.Reserved; *)
+			(*ftype		    = Sast.Reserved; *)
 		}
 	in
     let reserved_functions = [
         reserved_struct "print" (Datatype(Void));
     ] in
     reserved_functions
+    
+let get_class_maps cdecls = 
+    let setup_class_map m cdecl =
+        (* get all fields belonging to the class cdecl. Raise error if duplicates are found *)
+        let field_maps m = function Vdecl(typ, name) ->
+            if (StringMap.mem name m)
+                then raise (Failure(" duplicate field name :" ^ name))
+            else
+                (StringMap.add name (Vdecl(typ, name)) m)
+        in
 
+        (* get all methods belonging to the class cdecl. Raise error if duplicates are found *)
+        let func_maps m fdecl = 
+            let func_full_name = get_fully_qualified_name cdecl.cname fdecl
+            in
+            if (StringMap.mem func_full_name m)
+                then raise (Failure ("duplicate function : " ^ func_full_name))
+            else
+                (* check against reserved func maps here *)
+                (StringMap.add (func_full_name) fdecl m)
+        in
+
+        (* check for duplicate classes and add their fields, methods respectively *)
+        (if (StringMap.mem cdecl.cname m)
+            then raise(Failure ("Duplicate Class Name : " ^ cdecl.cname ))
+        else
+            StringMap.add cdecl.cname
+            {
+                field_map = List.fold_left field_maps StringMap.empty cdecl.cbody.fields;
+                func_map = List.fold_left func_maps StringMap.empty cdecl.cbody.methods;
+                cdecl = cdecl;
+            } 
+            m
+        )   
+    in List.fold_left setup_class_map StringMap.empty cdecls
+(*in*)
 let check pgm = function
     Program(cdecls) ->
         (* generate reserved functions and obtain their map *)
